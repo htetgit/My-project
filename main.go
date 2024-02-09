0616f5f8a25e418f18f276fd2d06ac74b90dffe0
htetgit <htetyemon@galaxie.ai>
2024-02-09 14:21:44 +0630

    Add no Change

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server started on port ::::9091")
+	http.ListenAndServe(":9091", nil)
+}
